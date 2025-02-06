e24a514ce398623e6294f86127e275631a1f8c50
Raul Petre Manea <raulpetrelm@gmail.com>
2025-02-06 14:49:28 +0100

    Añadido contador

---
+++ src/components/Contador.vue
@@ -0,0 +1,24 @@
+<template>
+ <h1>Hola Mundo</h1>
+ <p>{{ contador }}</p>
+ <button @click="incrementar_Uno">Incrementar Primer Valor</button>
+ <button @click="decrementar_Uno">Decrementar Primer Valor</button>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+
+const contador = ref(0)
+const incrementar_Uno = () => {
+  contador.value++;
+}
+const decrementar_Uno = () => {
+  contador.value--;
+}
+</script>
+
+<style scoped>
+p {
+  color: red;
+}
+</style>
